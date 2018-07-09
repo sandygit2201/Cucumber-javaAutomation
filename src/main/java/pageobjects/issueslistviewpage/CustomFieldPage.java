473d768f2d83ffa6244ca0f311985fcf8bd11860
Mohan D <mohand@aconex.com>
2018-07-09 15:16:01 +0530

    Refactor import

---
+++ src/main/java/pageobjects/issueslistviewpage/CustomFieldPage.java
@@ -0,0 +1,13 @@
+package pageobjects.issueslistviewpage;
+
+
+import pageobjects.BasePage;
+import java.io.IOException;
+
+public class CustomFieldPage extends BasePage {
+
+
+    public CustomFieldPage() throws IOException {
+        super();
+    }
+}
---
+++ src/main/java/steps/Issueslistviewsteps/customFieldSteps.java
@@ -0,0 +1,28 @@
+package steps.Issueslistviewsteps;
+
+import Utils.DriverFactory;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+
+public class customFieldSteps extends DriverFactory {
+    @And("^I click on the first custom field$")
+    public void iClickOnTheFirstCustomField() throws Throwable {
+
+    }
+
+    @And("^I select the value as High$")
+    public void iSelectTheValueAsHigh() throws Throwable {
+
+    }
+
+    @And("^I click on Refresh button$")
+    public void iClickOnRefreshButton() throws Throwable {
+
+    }
+
+    @Then("^the selected custom field value should be saved for that issue$")
+    public void theSelectedCustomFieldValueShouldBeSavedForThatIssue() throws Throwable {
+
+    }
+}
