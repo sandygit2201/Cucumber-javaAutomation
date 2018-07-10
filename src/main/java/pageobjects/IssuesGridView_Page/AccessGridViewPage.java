c143d58422d8ef36a70ef1fb18124707767a9519
Mohan D <mohand@aconex.com>
2018-07-10 13:58:23 +0530

    Fixed refactor conflicts

---
+++ src/main/java/pageobjects/IssuesGridView_Page/AccessGridViewPage.java
@@ -0,0 +1,39 @@
+package pageobjects.IssuesGridView_Page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import pageobjects.BasePage;
+
+import java.io.IOException;
+
+public class AccessGridViewPage extends BasePage {
+
+
+    public @FindBy(css = "div.display-mode > div.table-view-button")
+    WebElement button_GridView;
+    public @FindBy(css = "div.auiToolbar-right.ng-scope > button")
+    WebElement button_EditMode;
+
+    public AccessGridViewPage() throws IOException {
+        super();
+    }
+
+    public AccessGridViewPage preLoaderPage() throws InterruptedException, IOException {
+        waitUntilPreLoadElementDisappears(By.cssSelector("fm-app > div:nth-child(4) > div > div > div"));
+        return new AccessGridViewPage();
+    }
+
+    public AccessGridViewPage accessGridView() throws InterruptedException, IOException {
+        waitAndClickElement(button_GridView);
+        return new AccessGridViewPage();
+
+    }
+    public AccessGridViewPage verifytheGridView() throws InterruptedException, IOException {
+        button_EditMode.isDisplayed();
+        return new AccessGridViewPage();
+
+    }
+
+
+}
