56bb5ab465b88688dd77d4b5db3123345fcb1fff
Sujai Kamat <skamat@aconex.com>
2018-07-19 09:38:20 +0530

    Add standalone chrome image for local build

---
+++ local-build-chrome.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+docker-compose -f docker-compose-local-chrome.yaml up -d
+echo "Waiting for VNC to initialize..." && sleep 10
+
+open vnc://:secret@localhost:5900
+DRIVER=grid-chrome mvn clean test
+EXIT_CODE=$?
+docker-compose -f docker-compose-local-chrome.yaml down
+
+exit $EXIT_CODE
