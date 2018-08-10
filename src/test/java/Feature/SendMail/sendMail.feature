8bc626cad0a853499ca8c213bae72721f110c6cd
Sony Joseph <sjoseph@SJOSEPH-MBA.local>
2018-08-10 14:59:16 +0530

    Refractor send mail feature and step file

--- src/test/java/Feature/SendMail/sendMail.feature
+++ src/test/java/Feature/SendMail/sendMail.feature
@@ -6,33 +6,33 @@ Feature: User should be able to send Issues report though mail
     And User enters a valid password
     When User clicks on the login button
     Then User should be taken to the homepage
-    Given I click on the project list
-    And I select Web automation project
-    And I agree to Terms of Service Update if prompted
-    Then I should be taken to that project
+    Given User click on the project list
+    And User select Web automation project
+    And User agree to Terms of Service Update if prompted
+    Then User should be taken to that project
 
   Scenario: User should be able to send mail when there are assigned issues in an area
     When User clicks on Field-Issues
-    Then I should close the FTUX
-    And I click on BLOCK E area
-    Then I should close the area selector
+    Then User should close the FTUX
+    And User click on BLOCK E area
+    Then User should close the area selector
     Then User should see the issues page
     Given the Send button should be enabled
-    When I click on Send Button
-    Then I should see the Send mail modal
+    When User click on Send Button
+    Then User should see the Send mail modal
     And the Send button should be disabled
-    And I click on Mail type drop down
-    And I select Variation mail type
+    And User click on Mail type drop down
+    And User select Variation mail type
     Then the Send button in the modal should be enabled
-    And when I click on Send button
+    And User click on Send button
     Then the Send mail modal should disappear
 
   Scenario: User should see the sent mail in Mail module
     Given User clicks on Mail-Sent module
-    Then the user should land in the Mail module
-    And the user selects today from the date column
-    And the user clicks the Search button
-    Then the user should see the mail with subject: Aconex Field Issues for BLOCK E
+    Then User should land in the Mail module
+    And User selects today from the date column
+    And User clicks the Search button
+    Then User should see the mail with subject: Aconex Field Issues for BLOCK E
     And the From should be Maurice Jenner
     And the type should be Variation
 
--- src/test/java/steps/SendMailSteps/SendMailSteps.java
+++ src/test/java/steps/SendMailSteps/SendMailSteps.java
@@ -8,7 +8,7 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class SendMailSteps {
-    @And("^I click on BLOCK E area$")
+    @And("^User click on BLOCK E area$")
     public void iClickOnBLOCKEArea() throws Throwable {
         sendMail.selectArea();
     }
@@ -18,12 +18,12 @@ public class SendMailSteps {
         sendMail.verifySendButtonEnabled();
     }
 
-    @When("^I click on Send Button$")
+    @When("^User click on Send Button$")
     public void iClickOnSendButton() throws Throwable {
         sendMail.clickSendButtonInIssuesPage();
     }
 
-    @Then("^I should see the Send mail modal$")
+    @Then("^User should see the Send mail modal$")
     public void iShouldSeeTheSendMailModal() throws Throwable {
         sendMail.verifySendMailModalDisplayed();
     }
@@ -33,12 +33,12 @@ public class SendMailSteps {
         sendMail.verifySendButtonInModalDisabled();
     }
 
-    @And("^I click on Mail type drop down$")
+    @And("^User click on Mail type drop down$")
     public void iClickOnMailTypeDropDown() throws Throwable {
         sendMail.clickOnMailTypeDropDown();
     }
 
-    @And("^I select Variation mail type$")
+    @And("^User select Variation mail type$")
     public void iSelectVariationMailType() throws Throwable {
         sendMail.clickOnVariationMailType();
     }
@@ -48,7 +48,7 @@ public class SendMailSteps {
         sendMail.verifySendButtonInModalEnabled();
     }
 
-    @And("^when I click on Send button$")
+    @And("^User click on Send button$")
     public void whenIClickOnSendButton() throws Throwable {
         sendMail.clickSendButtonInModal();
 
@@ -65,22 +65,22 @@ public class SendMailSteps {
         sendMail.navigateToMailSentFolder();
     }
 
-    @Then("^the user should land in the Mail module$")
+    @Then("^User should land in the Mail module$")
     public void theUserShouldLandInTheMailModule() throws Throwable {
         sendMail.assertMailModule();
     }
 
-    @And("^the user selects today from the date column$")
+    @And("^User selects today from the date column$")
     public void theUserSortsTheDateFromRecentToOld() throws Throwable {
         sendMail.selectDateToToday();
     }
 
-    @And("^the user clicks the Search button$")
+    @And("^User clicks the Search button$")
     public void theUserClicksTheSearchButton() throws Throwable {
         sendMail.clickSearchButton();
     }
 
-    @Then("^the user should see the mail with subject: Aconex Field Issues for BLOCK E$")
+    @Then("^User should see the mail with subject: Aconex Field Issues for BLOCK E$")
     public void theUserShouldSeeTheMailWithSubjectAconexFieldIssuesForBLOCKE() throws Throwable {
         sendMail.assertMailSubject();
     }
