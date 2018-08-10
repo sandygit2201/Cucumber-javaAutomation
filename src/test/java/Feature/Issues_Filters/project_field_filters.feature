4cd100174c9f8a0915e83abbd40accff312d9e48
Sony Joseph <sjoseph@SJOSEPH-MBA.local>
2018-08-10 12:12:12 +0530

    Refractor project field filters feature and step file

--- src/test/java/Feature/Issues_Filters/project_field_filters.feature
+++ src/test/java/Feature/Issues_Filters/project_field_filters.feature
@@ -6,22 +6,22 @@ Feature: Toggle Project field filters
     And User enters a valid password
     When User clicks on the login button
     Then User should be taken to the homepage
-    Given I click on the project list
-    And I select Web automation project
-    And I agree to Terms of Service Update if prompted
-    Then I should be taken to that project
+    Given User click on the project list
+    And User select Web automation project
+    And User agree to Terms of Service Update if prompted
+    Then User should be taken to that project
     When User clicks on Field-Issues
-    And I should close the FTUX
-    Then I should close the area selector
+    And User should close the FTUX
+    Then User should close the area selector
 
   Scenario: User should be able to apply project field filters
       """
       Select "Priority" CC and select "High" value
       """
-      Given I click on Extra details
-      Then I should see the Extra details modal
-      And I select the project field
-      When I select one value in that project field
-      And I click on Apply button
-      Then I should see the loading screen
-      Then I should see the result as per the filter
+      Given User click on Extra details
+      Then User should see the Extra details modal
+      And User select the project field
+      When User select one value in that project field
+      And User click on Apply button
+      Then User should see the loading screen
+      Then User should see the result as per the filter
--- src/test/java/steps/IssuesFilters_Steps/ProjectFieldFiltersSteps.java
+++ src/test/java/steps/IssuesFilters_Steps/ProjectFieldFiltersSteps.java
@@ -8,27 +8,27 @@ import cucumber.api.java.en.When;
 import static Utils.DriverFactory.projectFieldFilter;
 
 public class ProjectFieldFiltersSteps {
-    @Given("^I click on Extra details$")
+    @Given("^User click on Extra details$")
     public void iClickOnExtraDetails() throws Throwable {
         projectFieldFilter.clickOnExtraDetails();
     }
 
-    @Then("^I should see the Extra details modal$")
+    @Then("^User should see the Extra details modal$")
     public void iShouldSeeTheExtraDetailsModal() throws Throwable {
         projectFieldFilter.viewExtraDetailsModal();
     }
 
-    @And("^I select the project field$")
+    @And("^User select the project field$")
     public void iSelectTheProjectField() throws Throwable {
         projectFieldFilter.clickOnFirstProjectField();
     }
 
-    @When("^I select one value in that project field$")
+    @When("^User select one value in that project field$")
     public void iSelectOneValueInThatProjectField() throws Throwable {
         projectFieldFilter.selectFirstValue();
     }
 
-    @And("^I click on Apply button$")
+    @And("^User click on Apply button$")
     public void iClickOnApplyButton() throws Throwable {
         projectFieldFilter.clickApplyButton();
     }
@@ -38,7 +38,7 @@ public class ProjectFieldFiltersSteps {
         projectFieldFilter.assertLoadingScreen();
     }
 
-    @Then("^I should see the result as per the filter$")
+    @Then("^User should see the result as per the filter$")
     public void iShouldSeeTheResultAsPerTheFilter() throws Throwable {
         projectFieldFilter.assertProjectFieldFilterResult();
     }
