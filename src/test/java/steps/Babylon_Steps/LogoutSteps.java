5166615c73f32c06e28ee8be6f6ddf27ee260c84
Sony Joseph <sjoseph@SJOSEPH-MBA.local>
2018-09-05 12:01:04 +0530

    Refractor Logout step file

--- src/test/java/steps/Babylon_Steps/LogoutSteps.java
+++ src/test/java/steps/Babylon_Steps/LogoutSteps.java
@@ -8,12 +8,12 @@ import cucumber.api.java.en.When;
 public class LogoutSteps extends DriverFactory {
 
     @When("^user clicks on logout link$")
-    public void user_clicks_on_logout_link() throws Throwable {
+    public void userClicksOnLogoutLink() throws Throwable {
     logout.clickLogout();
     }
 
     @Then("^user should be successfully logged out$")
-    public void user_should_be_successfully_logged_out() throws Throwable {
+    public void userShouldBeSuccessfullyLoggedOut() throws Throwable {
         logout.successfulLogout();
     }
 }
