ccad9a79b9574d8dcb2941947307a04606ab6ebd
Mohan D <mohand@aconex.com>
2018-10-18 14:47:03 +0530

    Setting pom path inside docker

--- docker-scripts/quick-build.sh
+++ docker-scripts/quick-build.sh
@@ -5,7 +5,7 @@ DOWNLOAD_DIR=/tmp/fieldwebautomation/
 mkdir -p $DOWNLOAD_DIR
 chmod -R a+rw $DOWNLOAD_DIR
 docker-compose up -d
-DRIVER=grid-chrome mvn clean test
+DRIVER=grid-chrome mvn -f ../pom.xml clean test
 EXIT_CODE=$?
 docker-compose down
 
