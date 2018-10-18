332ef74358e5dc72da1b4bbbe873993130142b17
Mohan D <mohand@aconex.com>
2018-10-18 15:03:43 +0530

    Corrected file location

--- src/test/java/com/aconex/fieldautomation/runner/MainRunner.java
+++ src/test/java/com/aconex/fieldautomation/runner/MainRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/java/com.aconex.fieldautomation/feature"},
+        features = {"src/test/java/com/aconex/fieldautomation/feature"},
         glue = {"com.aconex.fieldautomation/steps"},
         monochrome = true,
         plugin = {"pretty", "html:target/cucumber/html", "json:target/cucumber/cucumber.json"}
